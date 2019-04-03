9b9a0705f56d1ceb55f8433878a4a33ce56cf8c6
bthornton191 <ben.thornton@mscsoftware.com>
2019-04-03 11:30:06 -0500

    added a SurveyData class

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dripy",
-    version="0.0.14",
+    version="0.1.0",
     author="Ben Thornton",
     author_email="ben.thornton@mscsoftware.com",
     description="Python tools for working with drilling data",
