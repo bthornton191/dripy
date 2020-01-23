5074af0e35e50f84be6dfc4118939d9d3a99b1ea
Ben Thornton <ben.thornton@mscsoftware.com>
2020-01-23 09:15:29 -0500

    Added class for lodestar data.  Added support for Corva. Updated version number.

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -1,6 +1,6 @@
 # ---------------------
-version = '0.2'
-version_notes = 'Added class for lodestar data.  Added support for Corva.'
+version = '0.2.1'
+version_notes = 'Added class for lodestar data.  Added support for Corva. Updated version number.'
 date = 'January 23rd, 2020'
 # ---------------------
 author = 'Ben Thornton'
